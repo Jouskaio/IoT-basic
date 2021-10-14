e03e93113eeb67b3faf20c9dab6c9bcf1de8f42c
Jouskaio <jouskaio.me@gmail.com>
2021-10-14 17:07:44 +0200

    Final script to visualize data from broker

--- check_topic.sh
+++ check_topic.sh
@@ -3,8 +3,15 @@
 # Ask for informations : the borker adress and the topic
 echo "Script to check if this topic exists on your broker. Please, answer to the next questions."
 read -p "Broker adress (IP or link) : " adress
-read -p "Topic asked :" topic
+read -p "Topic asked : " topic
 
-# Check if the mosquitto_sub is install and actif
-#if eval($(mosquitto_sub --help) )
-
+# Check if the mosquitto_sub is active
+if [ $(pgrep mosquitto | wc -c) -gt 0 ]
+then
+    mosquitto_sub -h ${adress} -t "${topic}" | while read -r response
+    do
+        echo " $adress : $topic      $response"
+    done
+else
+    echo "The mosquitto service isn't active, please start it with systemctl start mosquitto"
+fi
